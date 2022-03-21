b9e0c4cec3efbbd2fdc1fbacf5086cc12c409b87
WindChenx <190235290@qq.com>
2022-03-22 00:11:17 +0800

    移除权限申请框架 删除重复的图片uri地址获取

--- app/src/main/java/com/example/thatgirl/utils/FileUtils.java
+++ app/src/main/java/com/example/thatgirl/utils/FileUtils.java
@@ -262,88 +262,67 @@ public class FileUtils {
         if (context == null || uri == null) {
             return null;
         }
+
         // DocumentProvider
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
             if (isExternalStorageDocument(uri)) {
                 final String docId = DocumentsContract.getDocumentId(uri);
                 final String[] split = docId.split(":");
                 final String type = split[0];
+
                 if ("primary".equalsIgnoreCase(type)) {
                     return Environment.getExternalStorageDirectory() + "/" + split[1];
-                } else if ("home".equalsIgnoreCase(type)) {
-                    return Environment.getExternalStorageDirectory() + "/documents/" + split[1];
-                } else {
-                    return "/storage/" + type + "/" + split[1];
                 }
-            } else if (isDownloadsDocument(uri)) {
-                // DownloadsProvider
+
+                // TODO handle non-primary volumes
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
                 final String id = DocumentsContract.getDocumentId(uri);
-                if (TextUtils.isEmpty(id)) {
-                    return null;
-                }
-                if (id.startsWith("raw:")) {
-                    return id.substring(4);
-                }
-                String[] contentUriPrefixesToTry = new String[]{
-                        "content://downloads/public_downloads",
-                        "content://downloads/my_downloads",
-                        "content://downloads/all_downloads"
-                };
-                for (String contentUriPrefix : contentUriPrefixesToTry) {
-                    try {
-                        Uri contentUri = ContentUris.withAppendedId(Uri.parse(contentUriPrefix), Long.valueOf(id));
-                        String path = getDataColumn(context, contentUri, null, null);
-                        if (path != null) {
-                            return path;
-                        }
-                    } catch (Exception ignore) {
-                    }
-                }
-                try {
-                    String path = getDataColumn(context, uri, null, null);
-                    if (path != null) {
-                        return path;
-                    }
-                } catch (Exception ignore) {
-                }
-                // path could not be retrieved using ContentResolver, therefore copy file to accessible cache using streams
-                return null;
-            } else if (isMediaDocument(uri)) {
-                // MediaProvider
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
                 final String docId = DocumentsContract.getDocumentId(uri);
                 final String[] split = docId.split(":");
                 final String type = split[0];
 
-                Uri contentUri;
-                switch (type.toLowerCase(Locale.ENGLISH)) {
-                    case "image":
-                        contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-                        break;
-                    case "video":
-                        contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
-                        break;
-                    case "audio":
-                        contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
-                        break;
-                    default:
-                        contentUri = MediaStore.Files.getContentUri("external");
-                        break;
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
                 }
-                final String selection = MediaStore.MediaColumns._ID + "=?";
-                final String[] selectionArgs = new String[]{split[1]};
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[] {
+                        split[1]
+                };
+
                 return getDataColumn(context, contentUri, selection, selectionArgs);
             }
-        } else if (ContentResolver.SCHEME_CONTENT.equalsIgnoreCase(uri.getScheme())) {
-            // MediaStore (and general)
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+
             // Return the remote address
-            if (isGooglePhotosUri(uri)) {
+            if (isGooglePhotosUri(uri))
                 return uri.getLastPathSegment();
-            }
+
             return getDataColumn(context, uri, null, null);
-        } else if (ContentResolver.SCHEME_FILE.equalsIgnoreCase(uri.getScheme())) {
-            // File
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
             return uri.getPath();
         }
+
         return null;
     }
 
@@ -357,22 +336,22 @@ public class FileUtils {
      * @return 路径，未找到返回null
      */
     public static String getDataColumn(Context context, @NonNull Uri uri, String selection, String[] selectionArgs) {
+        String path = null;
+
+        String[] projection = new String[]{MediaStore.Images.Media.DATA};
         Cursor cursor = null;
-        String column = MediaStore.Images.Media.DATA;
-        String[] projection = {column};
         try {
             cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs, null);
             if (cursor != null && cursor.moveToFirst()) {
-                int index = cursor.getColumnIndexOrThrow(column);
-                return cursor.getString(index);
+                int columnIndex = cursor.getColumnIndexOrThrow(projection[0]);
+                path = cursor.getString(columnIndex);
             }
-        } catch (Exception ignore) {
-        } finally {
+        } catch (Exception e) {
             if (cursor != null) {
                 cursor.close();
             }
         }
-        return null;
+        return path;
     }
 
     public static boolean isExternalStorageDocument(Uri uri) {
